670a98c8fcf350b1c99d7ed5d07456972c15c004
FlowWind1999 <1832082859@qq.com>
2019-11-01 19:06:48 +0800

    P1046.cpp

---
+++ P1046.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	vector<int> apples;
+	for(int i=0;i!=10;++i){
+		int temp;
+		cin>>temp;
+		apples.push_back(temp);
+	}
+	int high;
+	cin>>high;
+	high+=30;
+	sort(apples.begin(),apples.end());
+	int count=0;
+	for(int i=0;i!=10;++i){
+		if(apples[i]<=high)
+			count++;
+		else
+			break;
+	}
+	cout<<count;
+	return 0;
+}
