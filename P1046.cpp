1f83f15baa25393cbc1d0301923eb0631d10b231
FlowWind1999 <1832082859@qq.com>
2019-11-01 20:11:18 +0800

    delete

--- P1046.cpp
+++ P1046.cpp
@@ -12,12 +12,7 @@ int main(){
 	high+=30;
 	sort(apples.begin(),apples.end());
 	int count=0;
-	for(int i=0;i!=10;++i){
-		if(apples[i]<=high)
-			count++;
-		else
-			break;
-	}
+	
 	cout<<count;
 	return 0;
 }
